c2e28fd26d7d6373525e0e41330a6bbe7e8cf7e2
Callep3 <42645914+Callep3@users.noreply.github.com>
2021-10-12 17:00:11 +0200

    Added CreatureSight

--- Assets/Scripts/Creatures/CreatureSight.cs
+++ Assets/Scripts/Creatures/CreatureSight.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,11 +6,54 @@ using UnityEngine;
 public class CreatureSight : MonoBehaviour
 {
     [Header("Settings")] 
-    [SerializeField] private LayerMask obstacleLayers;
-    [SerializeField] private LayerMask targetLayers;
-    [SerializeField] private float sightRange;
-    [SerializeField] private float sightAngle;
+    [SerializeField] private LayerMask obstacleMask;
+    [SerializeField] private LayerMask targetMask;
+    [SerializeField] public float viewRadius;
+    [SerializeField, Range(0, 360)] public float viewAngle;
     
+    public List<Transform> visibleTargets = new List<Transform>();
+
+    private void Start()
+    {
+        StartCoroutine(FindTargetsWithDelay(.2f));
+    }
+
+    private IEnumerator FindTargetsWithDelay(float delay)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+            FindVisibleTargets();
+        }
+    }
     
+    private void FindVisibleTargets()
+    {
+        visibleTargets.Clear();
+        Collider2D[] targetsInViewRadius = Physics2D.OverlapCircleAll(transform.position, viewRadius, targetMask);
+
+        for (int i = 0; i < targetsInViewRadius.Length; i++)
+        {
+            Transform target = targetsInViewRadius[i].transform;
+            Vector2 directionToTarget = (target.position - transform.position).normalized;
+            if (Vector2.Angle(transform.up, directionToTarget) < viewAngle / 2)
+            {
+                float distanceToTarget = Vector2.Distance(transform.position, target.position);
+
+                if (!Physics2D.Raycast(transform.position, directionToTarget, distanceToTarget, obstacleMask))
+                {
+                    visibleTargets.Add(target);
+                }
+            }
+        }
+    }
     
+    public Vector2 DirectionFromAngle(float angleInDegrees, bool angleIsGlobal)
+    {
+        if (!angleIsGlobal)
+        {
+            angleInDegrees -= transform.eulerAngles.z;
+        }
+        return new Vector2(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
 }
