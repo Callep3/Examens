beae84615d48e38fed7e0e88c930a222c492e24a
Callep3 <42645914+Callep3@users.noreply.github.com>
2021-10-14 16:21:41 +0200

    Updated CreatureCharacteristics defaults

--- Assets/Scripts/Creatures/CreatureCharacteristics.cs
+++ Assets/Scripts/Creatures/CreatureCharacteristics.cs
@@ -5,14 +5,26 @@ using UnityEngine;
 
 public class CreatureCharacteristics : MonoBehaviour
 {
-    [SerializeField] private float food;
-    [SerializeField] private float water;
-    [SerializeField] private float energy;
-    [SerializeField] private float health;
+    [Header("Stats")]
+    public float food;
+    public float maxFood = 100f;
+    public float water;
+    public float maxWater = 100f;
+    public float energy;
+    public float maxEnergy = 100f;
+    public float health;
+    public float maxHealth = 100f;
+    
+    [Header("Characteristics")] 
+    public bool herbivore;
+    public bool carnivore;
 
     private void Start()
     {
-        
+        food = maxFood;
+        water = maxWater;
+        energy = maxEnergy;
+        health = maxHealth;
     }
 
     private void Update()
