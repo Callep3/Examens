32b4dc16bd67695a9e62d1829960586a3bf94bc1
Callep3 <42645914+Callep3@users.noreply.github.com>
2021-10-14 15:53:03 +0200

    Added a general script for the creatures characteristics

---
+++ Assets/Scripts/Creatures/CreatureCharacteristics.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatureCharacteristics : MonoBehaviour
+{
+    [SerializeField] private float food;
+    [SerializeField] private float water;
+    [SerializeField] private float energy;
+    [SerializeField] private float health;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+}
